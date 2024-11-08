b5373fb9d2ad0cffb4a6c321881dd2fe1ea1ebe3
Nachatra Sharma <116380986+nachatra-sharma@users.noreply.github.com>
2024-11-08 13:29:53 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,6 +19,7 @@ const Header = () => {
   } = useContext(userContext);
 
   const { isMenuOpen, handleMenu } = useContext(menuContext);
+  console.log("checking menu", isMenuOpen);
 
   useEffect(() => {
     function checkForCookies() {
@@ -50,6 +51,7 @@ const Header = () => {
   const loggedInUser = getLoggedInUser();
 
   const toggleMenu = () => {
+    console.log(!isMenuOpen);
     handleMenu(!isMenuOpen);
   };
 
